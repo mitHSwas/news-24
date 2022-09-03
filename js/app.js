f29c9f16e19a2c77ec4c9870df0c33d3dfbd3541
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-09-03 17:05:27 +0600

    need bootstrap icon

--- js/app.js
+++ js/app.js
@@ -30,20 +30,30 @@ const loadCategoryNews = newsId => {
 
 const displayCategoryNews = categoryNews => {
     const newsContainer = document.getElementById("news-container");
+    newsContainer.textContent = ``;
     categoryNews.forEach(news => {
+        console.log(news)
         const newsDiv = document.createElement("div");
         newsDiv.classList.add("card", "mb-3");
         newsDiv.innerHTML = `
             <div class="row g-0">
-                <div class="col-md-3">
-                    <img src="..." class="img-fluid rounded-start" alt="...">
+                <div class="col-md-4">
+                    <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
                 </div>
-                <div class="col-md-9">
+                <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <h5 class="card-title">${news.title}</h5>
+                        <p class="card-text">${news.details.slice(0, 250)}....</p>
+                        <div class="d-flex">
+                            <div class="d-flex ">
+                                <img class="author-img" src="${news.author.img}">
+                                <div>
+                                    <h6>${news.author.name}</h6>
+                                    <p>${news.author.published_date}</p>
+                                </div>
+                            </div>
+                            <p><p>
+                        </div>
                     </div>
                 </div>
             </div>
